6f0fc597a4e8baa26bfdbe85f1f5c6b2bd3c712d
dimateg <dimateg@gmail.com>
2022-04-18 16:40:07 +0300

    new dir

--- json-parsing.py
+++
@@ -1,8 +0,0 @@
-import json
-
-json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
-
-media = json.loads(json_text)
-
-pars_json = media['messages'][1]['message']
-print(pars_json)
--- redirect.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-
-redirect = response.history
-redirect_final = response
-
-print(redirect.__len__())
-print(redirect_final.url)
