70a948ee5839f9436e6b78b9157a2acd4509eb22
dimateg <dimateg@gmail.com>
2022-04-20 11:18:37 +0300

    <15

--- all_py/short_phrase.py
+++ all_py/short_phrase.py
@@ -2,4 +2,4 @@ class TestShortPhrase:
     def test_phrase_length(self):
         phrases = input("Set a phrase: ")
         sums = len(phrases)
-        assert sums <= 15, f"Phrase length is not 15 characters {sums}"
+        assert sums < 15, f"Phrase length is not 15 characters {sums}"
